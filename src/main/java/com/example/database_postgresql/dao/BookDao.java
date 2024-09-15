98a1865a3de4f04400e1cdc5d7112171b7628420
M4ks1m123 <maximalian67@gmail.com>
2024-09-15 19:17:10 +0300

    Revert "dao and tests"
    This reverts commit c4e21fdd9a4ff88dacfb1db3a39d27b194f1b1b7.

--- src/main/java/com/example/database_postgresql/dao/BookDao.java
+++ src/main/java/com/example/database_postgresql/dao/BookDao.java
@@ -2,17 +2,6 @@ package com.example.database_postgresql.dao;
 
 import com.example.database_postgresql.domain.Book;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface BookDao {
     void create(Book book);
-
-    Optional<Book> findOne(String isbn);
-
-    List<Book> find();
-
-    void update(String isbn, Book book);
-
-    void delete(String isbn);
 }
