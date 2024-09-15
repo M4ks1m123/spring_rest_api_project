8d28dd0e05eedf18c7531b482069aed7136411f5
M4ks1m123 <maximalian67@gmail.com>
2024-09-15 17:53:21 +0300

    dao and tests

--- src/main/java/com/example/database_postgresql/dao/AuthorDao.java
+++ src/main/java/com/example/database_postgresql/dao/AuthorDao.java
@@ -2,6 +2,17 @@ package com.example.database_postgresql.dao;
 
 import com.example.database_postgresql.domain.Author;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AuthorDao {
     void create(Author author);
+
+    Optional<Author> findOne(long l);
+
+    List<Author> find();
+
+    void update(long id, Author author);
+
+    void delete(long l);
 }
